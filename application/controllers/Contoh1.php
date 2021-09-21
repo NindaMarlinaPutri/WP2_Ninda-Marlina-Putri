2a61721cc819081a766d84fa2180c2c8915b2e36
NindaMarlinaPutri <nindamarlina.03@gmail.com>
2021-09-21 17:33:25 +0700

    project pertama

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	
+	public function index()
+	{
+		echo "<h1>Biodata</h1>";
+		echo "Perkenalkan nama saya Ninda Marlina Putri saya tinggal di daerah Taman Cimanggu Bogor<br>Saya merupakan salah satu Mahasiswa Semester 3 di Universitas Bina Sarana Informatika Jurusan Sistem Informasi Kelas 12.3A.13";
+	}
+}
+
+?>
